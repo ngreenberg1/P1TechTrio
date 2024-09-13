ff8c177e0ac6683b3f7d13df911910ae7e1deb32
Naomi Greenberg <ngreenberg@luc.edu>
2024-09-13 15:22:13 -0400

    added test suite and plateau class

---
+++ Plateau.java
@@ -0,0 +1,19 @@
+
+public class Plateau implements IPlateau {
+    /** x-coordinate for map */
+    private int dimX;
+
+    /** y-coordinate for map */
+    private int dimY;
+
+
+    /** Basic constructor */
+    public Plateau(int dimX, dimY) {
+        this.dimX = dimX;
+        this.dimY = dimY;
+    }
+
+
+    //Not sure if/what we need to add to this
+
+} // class Plateau
